2663eb568e67513200f54229848a57eb1db72ea0
zeekhoks <106755622+zeekhoks@users.noreply.github.com>
2024-06-08 17:15:38 -0400

    new controller changes

--- app/main.go
+++ app/main.go
@@ -4,57 +4,41 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/zeekhoks/test-api-backend/routes"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/zeekhoks/test-api-backend/services"
 	"golang.org/x/net/context"
 	"log"
 	"os"
 )
 
-func goDotEnvVariable(key string) string {
+func main() {
 
-	// load .env file
-	err := godotenv.Load(".env")
-
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalln("No .env file available")
 	}
-
-	return os.Getenv(key)
-}
-
-var uri = goDotEnvVariable("MONGODB_URI")
-
-var mongoClient *mongo.Client
-
-func init() {
-	if err := connectToMongodb(); err != nil {
-		log.Fatal("Could not connect to MongoDB")
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatalln("Mongodb URI string not found")
+	}
+	// connecting to the database
+	client := services.Connection(uri)
+	if client == nil {
+		log.Fatalln("Failed to connect to MongoDB")
 	} else {
 		log.Println("Connected to MongoDB")
 	}
-}
-
-func main() {
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatalln(err)
+			return
+		}
+	}()
 
 	router := routes.GetRouter()
 
-	err := router.Run(":8000")
+	err = router.Run(":8000")
 
 	if err != nil {
 		fmt.Printf("Fatal error has occured: %v\n", err)
 	}
-}
-
-func connectToMongodb() error {
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
-	client, err := mongo.Connect(context.TODO(), opts)
-	if err != nil {
-		panic(err)
-	}
-	err = client.Ping(context.TODO(), nil)
-	mongoClient = client
-	return err
 }
