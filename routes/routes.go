24e53347629a912a3b2f8d2494228b76a0cff2b1
Raja Shekar Reddy Siriganagari <106755622+zeekhoks@users.noreply.github.com>
2024-06-09 17:35:20 -0400

    patch: Update routes and middleware

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,13 @@ func GetRouter() *gin.Engine {
 
 	router := gin.Default()
 
-	router.GET("/questions", middleware.BasicAuth(), controllers.GetDisplayQuestionsByTopicHandler())
+	router.GET("/questions", middleware.AdminAuth(), controllers.GetDisplayQuestionsByTopicHandler())
 	router.POST("/questions", middleware.AdminAuth(), controllers.UploadQuestionHandler())
 	router.GET("/topics", middleware.BasicAuth(), controllers.GetAllTopics())
 	router.POST("/user", controllers.CreateNewUser())
-	router.POST("/quiz", controllers.GenerateQuizHandler())
-	router.POST("/quiz/:id/response", controllers.SubmitAnswerHandler())
-	router.GET("/quiz/:id/result", controllers.QuizResultHandler())
+	router.POST("/quiz", middleware.BasicAuth(), controllers.GenerateQuizHandler())
+	router.POST("/quiz/:id/response", middleware.BasicAuth(), controllers.SubmitAnswerHandler())
+	router.GET("/quiz/:id/result", middleware.BasicAuth(), controllers.QuizResultHandler())
 
 	return router
 }
